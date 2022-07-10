6e32b0585d03b97ac69a04cd2349a3d245aabe52
alex <alex@zen.WOrKGROUP>
2022-07-10 14:57:40 +0500

    +version

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include "includes.h"
 #include "lib/lib_hello/lib_hello.h"
+#include "version.h"
 
 int main(int argc, char *argv[]) {
+#ifdef PROJECT_VERSION
+  printf("Hello World xD v. %d.%d.%d\n", PROJECT_VERSION_MAJOR,
+         PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH);
+#endif
   lib_hello hey("Hello world!");
   return 0;
 }
